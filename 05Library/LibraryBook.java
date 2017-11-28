70d34ce51cb1d32c7ca7ff19bcda329cc645630d
Andy Zhou <azhou4@stuy.edu>
2017-11-28 15:12:26 -0500

    Added Book.java and LibraryBook.java. Didn't finish LibraryBook toString

---
+++ 05Library/LibraryBook.java
@@ -0,0 +1,18 @@
+abstract Class LibraryBook extends Book implements Comparable<LibraryBook>{
+    private String callNumber;
+    public LibraryBook(String author,String title,String ISBN,String callNum){
+	super();
+	callNumber = callNum;
+    }
+    public String getCallNum(){return callNumber;}
+    public void setCallNum(String callNum){callNumber = callNum;}
+    abstract void checkout(String patron, String due);
+    abstract void returned();
+    abstract String circulationStatus();
+    public int compareTo(LibraryBook other){
+	return getCallNum().compareTo(other.getCallNum());
+    }
+    public String toString(){
+	return super.toString();
+    }
+}
