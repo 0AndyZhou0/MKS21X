f01a73dde04837bbbabb16b51d2c8358c8201849
Andy Zhou <azhou4@stuy.edu>
2017-12-02 16:20:56 -0500

    made most of superArrayIterator

--- 06SuperArrayIterator/SuperArrayIterator.java
+++ 06SuperArrayIterator/SuperArrayIterator.java
@@ -1,3 +1,21 @@
+import java.util.*;
+import java.io.*;
 public class SuperArrayIterator implements Iterator<String>{
-    
+    int current, end;
+    public SuperArrayIterator(int start,int end){
+	current = start;
+	this.end = end;
+    }
+    public String next(){
+	if (hasNext()){
+	    current++;
+	}
+	else{
+	    System.exit(1);
+	}
+	return current - 1;
+    }
+     public boolean hasNext(){
+	 return current <= end;
+     }
 }
