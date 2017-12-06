580c631f36c32aa20a278bebcecfbf8232a62b88
Andy Zhou <azhou4@stuy.edu>
2017-12-06 15:29:46 -0500

    selection sort

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -1,4 +1,13 @@
 public class Sorts{
+    public static void main(String[] args){
+	int[] a = new int[10];
+	for (int i = 0;i < a.length;i++){
+	    a[i] = (int)(Math.random()*100);
+	}
+	selectionSort(a);
+	System.out.println(a);
+    }
+    
     public static String name(){
 	return "10.Zhou.Andy"; 
     }
@@ -8,6 +17,36 @@ public class Sorts{
      *@param data  the elements to be sorted.
      */
     public static void selectionSort(int[] data){
-	
+	for (int i = 0;i < data.length;i++){
+	    int min = data[i];
+	    for (int n = i;n < data.length;n++){
+		if (data[n] < min){
+		    min = data[n];
+		}
+	    }
+	    data[i] = min;
+	}
+    }
+
+    // Checks if array is in order
+    // For testing
+    public static boolean isSorted(int[] ary){
+	for (int i = 0;i < ary.length - 1;i++){
+	    if (ary[i] > ary[i+1]){
+		return false;
+	    }
+	}
+	return true;
+    }
+
+    public String toString(int[] ary){
+	if (ary.length < 1){
+	    return "[]";
+	}
+	String str = "[";
+	for (int i = 0;i < ary.length;i++){
+	    str += ary[i] + ", ";
+	}
+	return str.substring(0,str.length() - 2) + "]";
     }
 }
